c50bdf9143b8fbfb7fcc995bdc16ec384425dbaa
Jonathan Schwartz <jonbs98@gmail.com>
2020-07-29 16:42:58 -0400

    Finished patterned message function, going to skip the getEvalSteps problem because I did a very similar problem earlier this summer

---
+++ hw3.py
@@ -0,0 +1,55 @@
+def patternedMessage(msg, patt):
+    #loop through message and replace all non-whitespace or newline characters with message
+
+    #remove whitespace from message
+    msg = msg.replace(" ", "")
+
+    #current position in string msg
+    pos = 0
+    #use this to avoid index out of range error
+    maxPos = len(msg)
+
+    for i in range(len(patt)):
+        if (patt[i] != " " and patt[i] != "\n"):
+            patt = patt[:i] + msg[pos] + patt[i+1:]
+            pos += 1
+            if pos == maxPos:
+                pos=0
+
+    # removing intial or trailing whitespace
+    while(patt[0] == "\n"):
+        patt = patt[1:]
+    while(patt[-1] == "\n"):
+        patt = patt[:-1]
+    print(patt)
+
+
+patternedMessage("Go Steelers!",
+"""
+                          oooo$$$$$$$$$$$$oooo
+                      oo$$$$$$$$$$$$$$$$$$$$$$$$o
+                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$
+   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$
+oo $ $ '$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$
+'$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$
+  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$
+  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  '$$$
+   '$$$'$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     '$$$
+    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     '$$$o
+   o$$'   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o
+   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$' '$$$$$$ooooo$$$$o
+  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$
+  $$$$$$$$'$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$'
+ ''''       $$$$    '$$$$$$$$$$$$$$$$$$$$$$$$$$$$'      o$$$
+            '$$$o     '$$$$$$$$$$$$$$$$$$'$$'         $$$
+              $$$o          '$$'$$$$$$'           o$$$
+               $$$$o                                o$$$'
+                '$$$$o      o$$$$$$o'$$$$o        o$$$$
+                  '$$$$$oo     '$$$$o$$$$$o   o$$$$'
+                     '$$$$$oooo  '$$$o$$$$$$$$$'
+                        '$$$$$$$oo $$$$$$$$$$
+                                '$$$$$$$$$$$
+                                    $$$$$$$$$$$$
+                                     $$$$$$$$$$'
+                                      '$$$'
+""")
