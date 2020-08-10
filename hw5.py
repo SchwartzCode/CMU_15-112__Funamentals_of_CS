334a86765b59c49515675c5b144c31a3cbad70c2
Jonathan Schwartz <jonbs98@gmail.com>
2020-08-10 15:43:10 -0400

    Setup basic board display with tkinter

--- hw5.py
+++ hw5.py
@@ -1,3 +1,6 @@
+import basic_graphics
+import tkinter as tk
+
 def areLegalValues(vals):
     # returns true if all elements in vals are valid sudoku values (integers 1-9, or 0 for blank space)
     for elem in vals:
@@ -80,14 +83,67 @@ test_doku = [
   [ 0, 0, 0, 0, 8, 0, 0, 7, 9 ]
 ]
 
-out = isLegalRow(test_doku, 1)
-print(out)
+smaller_test_doku = [
+  [ 5, 3, 0, 0],
+  [ 6, 0, 0, 1],
+  [ 0, 9, 8, 0],
+  [ 8, 0, 0, 0],
+]
 
-out2 = isLegalCol(test_doku, 1)
-print(out2)
+#note: I ran more tests when coding these helper functions but didn't bother
+#      writing up assert()s for each special case
+assert(isLegalRow(test_doku, 1) == True)
+assert(isLegalCol(test_doku, 1) == True)
+assert(isLegalBlock(test_doku, 4) == True)
+assert(isLegalSudoku(test_doku) == True)
 
-out3 = isLegalBlock(test_doku, 4)
-print(out3)
 
-out4 = isLegalSudoku(test_doku)
-print(out4)
+# ======== BEGINNING OF HOMEWORK 6 ==========
+TKroot = tk.Tk()
+
+
+
+
+def drawSudoku(canvas, width, height, board, margin):
+    # NOTE: Add a conditional to handle 4x4, 16x16 boards
+    test = tk.Canvas(canvas, width=width, height=height)
+    test.pack()
+
+    #draw border lines
+    borderWidth = 2.5
+    test.create_line(margin, margin, (width - margin), margin, width=borderWidth)
+    test.create_line(margin, margin, margin, (height - margin), width=borderWidth)
+    test.create_line(margin, (height - margin), (width - margin), (height - margin), width=borderWidth)
+    test.create_line( (width - margin), margin, (width - margin), (height - margin), width=borderWidth)
+
+    heightGap = (height - 2*margin) // len(board[0])
+    widthGap = (width - 2*margin) // len(board[0])
+
+
+
+    #drawing inner border lines
+    for i in range(1,len(board[0])):
+
+        if i%3 == 0:
+            #ensure every third line is darker to box in blocks
+            lineWidth = borderWidth
+        else:
+            lineWidth = 1
+
+        # vertical border line
+        test.create_line((margin + i*widthGap), margin, (margin + i*widthGap), (height - margin), width=lineWidth)
+        # horizontal split line
+        test.create_line(margin, (margin + i*heightGap), (width - margin), (margin + i*heightGap), width=lineWidth)
+
+    #test.create_text(margin + 0.5*widthGap, margin + 0.5*widthGap, text=board[0][0], fill="purple", font="Helvetica 12")
+
+
+    for i in range(len(board[0])):
+        for j in range(len(board[0])):
+            test.create_text(margin + (j+0.5)*widthGap, margin + (i+0.5)*widthGap, text=board[i][j], fill="purple", font="Helvetica 12")
+
+    TKroot.mainloop()
+
+
+drawSudoku(TKroot, 400, 400, test_doku, 20)
+#basic_graphics.run(width=400, height=600)
